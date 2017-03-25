cbfb631059e96d112e2f250c1651d8e28a0988c0
bitscuit <li_zhao_bin@hotmail.com>
2017-03-25 18:19:43 -0400

    Control unit finished. Has not been tested

--- Phase3/control_unit.vhd
+++ Phase3/control_unit.vhd
@@ -5,14 +5,14 @@ use ieee.std_logic_1164.all;
 
 entity control_unit is
 port(
-	clk, reset, stop, conff : in std_logic;
+	clk, reset, stop, conff_out : in std_logic;
 	IR : in std_logic_vector(31 downto 0);
 	
 	run, clr : out std_logic_vector;
 	inport_out, mem_read, mem_write : out std_logic;
 	hi_in, lo_in, con_in, pc_in, ir_in, y_in, z_in, mar_in,
-		mdr_in, outport_in, c_out, BA_out : out std_logic;
-	pc_out, mdr_out, zhi_out, zlo_out, hi_out, lo_out, r_in, r_out, gra, grb, grc : out std_logic;
+		mdr_in, outport_in, c_out, ba_out : out std_logic;
+	pc_out, mdr_out, zhi_out_sel, zlo_out_sel, hi_out, lo_out, r_in, r_out, gra, grb, grc : out std_logic;
 	sel_alu : in std_logic_vector(3 downto 0);
 	);
 end entity control_unit;
@@ -52,7 +52,7 @@ fsm : process(clk, reset)
 begin
 	if (reset = '1') then
 		present_state <= reset_state;
-	elsif (clk'EVENT and clk = '1') then
+	elsif (clk'EVENT and clk = '1' and run = '1') then
 		case present_state is
 			when reset_state =>
 				present_state <= fetch0;
@@ -361,154 +361,487 @@ begin
 end process fsm;
 
 fsm_logic : process(present_state)
+	variable prev_conff : std_logic := '0';
 begin
 	-- initialize signals
 	case present_state is
+		-- register control signals
+			gra			<= '0';
+			grb			<= '0';
+			grc			<= '0';
+			r_in			<= '0';
+			hi_in 		<= '0';
+			lo_in 		<= '0';
+			zhi_in 		<= '0';
+			zlo_in 		<= '0';
+			pc_in 		<= '0';
+			outport_in 	<= '0';
+			inport_in 	<= '0';
+			y_in 		<= '0';
+			ir_in 		<= '0';
+			-- bus control signals
+			r_out		<= '0';
+			hi_out 		<= '0';			
+			lo_out 		<= '0';			
+			zhi_out_sel <= '0';	
+			zlo_out_sel <= '0';	
+			pc_out 		<= '0';			
+			mdr_out 	<= '0';		
+			inport_out 	<= '0';	
+			c_out 		<= '0';
+			-- memory control signals
+			mdr_in 		<= '0';
+			mar_in		<= '0';
+			mem_read	<= '0';
+			mem_write	<= '0';
+			-- other
+			alu_in 		<= '0';
+			ba_out		<= '0';
+			con_in		<= '0';
+			
 		when reset_state =>
-		
+			clr 		<= '1', '0' after 5 ns;
+			
 		-- fetch instruction logic
 		when fetch0 =>
+			pc_out 		<= '1';
+			mar_in 		<= '1';
+			sel_alu 	<= "1110";
+			alu_in 		<= '1';
+			zhi_in 		<= '1';
+			zlo_in 		<= '1';
 		when fetch1 =>
+			zlo_out_sel <= '1';
+			pc_in 		<= '1';
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when fetch2 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when fetch3 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when fetch4 =>
-		
+			mdr_out 	<= '1';
+			ir_in		<= '1';
+			
 		-- load from memory logic
 		when ld_state0 =>
+			grb 		<= '1';
+			ba_out		<= '1';
+			y_in 		<= '1';
 		when ld_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in 		<= '1';
+			zhi_in		<= '1';
+			zlo_in		<= '1';
 		when ld_state2 =>
+			zlo_out_sel	<= '1';
+			mar_in 		<= '1';
 		when ld_state3 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ld_state4 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ld_state5 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ld_state6 =>
+			mdr_out		<= '1';
+			gra 		<= '1';
+			r_in 		<= '1';
 		
 		-- load immediate logic
 		when ldi_state0 =>
+			grb 		<= '1';
+			ba_out 		<= '1';
+			y_in 		<= '1';
 		when ldi_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in 		<= '1';
+			zhi_in 		<= '1';
+			zlo_in 		<= '1';
 		when ldi_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in 		<= '1';
 		
 		-- store into memory logic
 		when st_state0 =>
+			grb 		<= '1';
+			ba_out 		<= '1';
+			y_in 		<= '1';
 		when st_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in 		<= '1';
+			zhi_in 		<= '1';
+			zlo_in 		<= '1';
 		when st_state2 =>
+			zlo_out_sel	<= '1';
+			mar_in 		<= '1';
 		when st_state3 =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			mdr_in 		<= '1';
 		when st_state4 =>
+			mem_write 	<= '1';
 		when st_state5 =>
+			mem_write 	<= '1';
 		
 		-- load relative logic
 		when ldr_state0 =>
+			pc_out  	<= '1';
+			y_in 		<= '1';
 		when ldr_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in 		<= '1';
+			zhi_in 		<= '1';
+			zlo_in 		<= '1';
 		when ldr_state2 =>
+			zlo_out_sel <= '1';
+			mar_in 		<= '1';
 		when ldr_state3 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ldr_state4 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ldr_state5 =>
+			mem_read 	<= '1';
+			mdr_in 		<= '1';
 		when ldr_state6 =>
+			mdr_out 	<= '1';
+			gra 		<= '1';
+			r_in 		<= '1';
 		
 		-- store relative logic
 		when str_state0 =>
+			pc_out 		<= '1';
+			y_in 		<= '1';
 		when str_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in 		<= '1';
+			zhi_in		<= '1';
+			zlo_in 		<= '1';
 		when str_state2 =>
+			zlo_out_sel <= '1';
+			mar_in 		<= '1';
 		when str_state3 =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			mdr_in		<= '1';
 		when str_state4 =>
+			mem_write 	<= '1';
 		when str_state5 =>
+			mem_write 	<= '1';
 		
 		-- add logic
 		when add_state0 =>
+			grb 		<= '1';
+			r_out		<= '1';
+			y_in		<= '1';
 		when add_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when add_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- subtract logic
 		when sub_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when sub_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0001";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when sub_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- and logic
 		when and_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when and_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0100";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when and_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- or logic
 		when or_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when or_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0101";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when or_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- shift right logic
 		when shr_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when shr_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0111";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when shr_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- shift left logic
 		when shl_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when shl_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "1000";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when shl_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- rotate right logic
 		when ror_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when ror_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "1001";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when ror_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- rotate left logic
 		when rol_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when rol_state1 =>
+			grc 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "1010";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when rol_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- add immediate logic
 		when addi_state0 =>
+			grb 		<= '1';
+			r_out		<= '1';
+			y_in		<= '1';
 		when addi_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0000";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when addi_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- and immediate logic
 		when andi_state0 =>
+			grb 		<= '1';
+			r_out		<= '1';
+			y_in		<= '1';
 		when andi_state1 =>
+			c_out		<= '1';
+			sel_alu		<= "0100";
+			alu_in 		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when andi_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- or immediate logic
 		when ori_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when ori_state1 =>
+			c_out 		<= '1';
+			sel_alu 	<= "0101";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when ori_state2 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- multiply logic
 		when mul_state0 =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when mul_state1 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0010";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when mul_state2 =>
+			zlo_out_sel <= '1';
+			lo_in		<= '1';
 		when mul_state3 =>
+			zhi_out_sel <= '1';
+			hi_in		<= '1';
 		
 		-- divide logic
 		when div_state0 =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
 		when div_state1 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			sel_alu 	<= "0011";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when div_state2 =>
+			zlo_out_sel <= '1';
+			lo_in		<= '1';
 		when div_state3 =>
+			zhi_out_sel <= '1';
+			hi_in		<= '1';
 		
 		-- negate logic
 		when neg_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
+			sel_alu 	<= "1011";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when neg_state1 =>
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
 		
 		-- not logic
 		when not_state0 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			y_in 		<= '1';
+			sel_alu 	<= "0110";
+			alu_in		<= '1';
+			zlo_in		<= '1';
+			zhi_in		<= '1';
 		when not_state1 =>
-		
+			zlo_out_sel <= '1';
+			gra 		<= '1';
+			r_in		<= '1';
+			
 		-- branch logic
 		when br_state0 =>
+			gra 		<= '1';
+			r_out		<= '1';
+			con_in		<= '1';
+			prev_conff 	<= conff_out; 	-- a little hack since conff logic uses and gate for output
 		when br_state1 =>
+			grb 		<= '1';
+			r_out 		<= '1';
+			pc_in 		<= prev_conff and '1';
+			prev_conff 	<= '0';
 		
 		-- jump logic
 		when jr_state =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			pc_in 		<= '1';
 		
 		-- jump al logic
 		when jal_state0 =>
+			pc_out 		<= '1';
+			grb 		<= '1';
+			r_in 		<= '1';
 		when jal_state1 =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			pc_in 		<= '1;
 		
 		-- input logic
 		when in_state =>
+			inport_out 	<= '1';
+			gra 		<= '1';
+			r_in 		<= '1;
 		
 		-- output logic
 		when out_state =>
+			gra 		<= '1';
+			r_out 		<= '1';
+			outport_in 	<= '1';
 		
 		-- move from high logic
 		when mfhi_state =>
+			hi_out 		<= '1';
+			gra 		<= '1';
+			r_in 		<= '1';
 		
 		-- move from low logic
 		when mflo_state =>
+			lo_out 		<= '1';
+			gra 		<= '1';
+			r_in 		<= '1';
 		
 		-- no operation
 		when nop_state =>
@@ -516,6 +849,7 @@ begin
 		
 		-- halt logic
 		when halt_state =>
+			run 		<= '0';
 		
 		when others =>
 	end case;
